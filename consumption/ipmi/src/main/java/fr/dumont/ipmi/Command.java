c9edc2f88bb89b9aee8227b1a0eb1618fc35fe7f
Guillaume Le Louët <guillaume.lelouet@gmail.com>
2013-03-13 17:56:11 +0100

    modified the consumption drivers' pom to remove the upload of jar files.

--- consumption/ipmi/src/main/java/fr/dumont/ipmi/Command.java
+++ consumption/ipmi/src/main/java/fr/dumont/ipmi/Command.java
@@ -3,38 +3,38 @@ package fr.dumont.ipmi;
 /** commansd to send to IPMI through ssh */
 public enum Command {
 
-  /** get the list of sensors */
-  IPMI_SENSOR_LIST {
-    @Override
-    public String toArg(String... params) {
-      return " sensor list";
-    }
+	/** get the list of sensors */
+	IPMI_SENSOR_LIST {
+		@Override
+		public String toArg(String... params) {
+			return " sensor list";
+		}
 
-    @Override
-    public String[] toProcessBuilderArgs(String... params) {
-      String[] command = { "sensor", "list" };
-      return command;
-    }
-  },
+		@Override
+		public String[] toProcessBuilderArgs(String... params) {
+			String[] command = {"sensor", "list"};
+			return command;
+		}
+	},
 
-  /** get the value of one sensor */
-  IPMI_GET_SENSOR {
+	/** get the value of one sensor */
+	IPMI_GET_SENSOR {
 
-    @Override
-    public String toArg(String... params) {
-      return " sensor get '" + params[0] + "'";
-    }
+		@Override
+		public String toArg(String... params) {
+			return " sensor get '" + params[0] + "'";
+		}
 
-    @Override
-    public String[] toProcessBuilderArgs(String... params) {
-      String[] command = { "sensor", "get", params[0] };
-      return command;
-    }
+		@Override
+		public String[] toProcessBuilderArgs(String... params) {
+			String[] command = {"sensor", "get", params[0]};
+			return command;
+		}
 
-  };
+	};
 
-  public abstract String toArg(String... params);
+	public abstract String toArg(String... params);
 
-  public abstract String[] toProcessBuilderArgs(String... params);
+	public abstract String[] toProcessBuilderArgs(String... params);
 
 }
